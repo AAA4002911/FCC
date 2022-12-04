28a459c39d42b577362a82343d68c297f4ebb0f5
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-04 16:46:48 +0530

    initial commit

---
+++ javascript-codes/cashRegister.js
@@ -0,0 +1,107 @@
+/*
+Cash Register
+
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+
+See below for an example of a cash-in-drawer array:
+
+[
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]
+
+Tests
+
+Waiting: checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
+Waiting: checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+Waiting: checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+Waiting: checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+Waiting: checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+Waiting: checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+*/
+
+
+function checkCashRegister(price, cash, cid) {
+    var change = {};
+     var change_Needed = (cash - price).toFixed(2);
+    var total = parseFloat(cid.reduce((account, next) => { return account + next[1] }, 0.0)).toFixed(2);
+  
+    if (Number(total) === Number(change_Needed)) {
+      change.status = "CLOSED";
+      change.change = cid;
+    } else if (Number(total) < Number(change_Needed)) {
+      change.status = "INSUFFICIENT_FUNDS";
+      change.change = [];
+    } else {
+      const COIN = {
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.1,
+        "QUARTER": 0.25,
+        "ONE": 1,
+        "FIVE": 5,
+        "TEN": 10,
+        "TWENTY": 20,
+        "ONE HUNDRED": 100
+      }
+      change.status = "OPEN";
+      change.change = [];
+      for (let i = cid.length - 1; i >= 0; i--) {
+  
+        let coin_Name = cid[i][0];
+        let coin_Total = parseFloat(cid[i][1]);
+        let coin_Value = COIN[coin_Name];
+        let coin_Amount = (coin_Total / coin_Value).toFixed(2);
+        let cointoReturn = 0;
+  
+        while (change_Needed >= coin_Value && coin_Amount > 0) {
+          change_Needed -= coin_Value;
+          change_Needed = Math.round(change_Needed * 100) / 100;
+          coin_Amount--;
+          cointoReturn++;
+        }
+        if (cointoReturn > 0) {
+          change.change.push([coin_Name, cointoReturn * coin_Value]);
+        }
+      }
+      if (change_Needed !== 0) {
+        change.status = "INSUFFICIENT_FUNDS";
+        change.change = [];
+      }
+    }
+    return change;
+  }
+  function totalAmountCash(cid) {
+    return cid.reduce(function (previous, current) {
+      return previous + current[1];
+    }, 0)
+  }
+  
+  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);  
