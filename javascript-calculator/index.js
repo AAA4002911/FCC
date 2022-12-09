54980e8b4ef8049834c4026cd6ba6d62a636a65a
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-09 22:27:25 +0530

    final commit

--- javascript-calculator/index.js
+++ javascript-calculator/index.js
@@ -1,4 +1,3 @@
-
 function calculator(x, y, operator) {
     let result;
     switch (operator) {
@@ -31,47 +30,56 @@ let inputString = '';
 let num1 = 0;
 let num2 = 0;
 let operator = '';
+let equation = '' + num1;
+let operators = ["+", "-", "/", "*"];
 
 function input(val) {
     let len = inputString.length;
     if (val == '+' || val == '-' || val == '*' || val == '/') {
-        if (operator && val == '-') {
-            return num1 = num1 * (-1);
+        if (operators.indexOf(equation[equation.length - 1]) != -1) {
+            if (val == '-' && equation[equation.length - 1] == '*') { equation = '' + num1 + '*' + val}
+            else {equation = '' + num1 + val;}
+            operator = val;
+            inputString = '';
+            return;
         }
+        equation += val;
+
         num1 = parseFloat(inputString);
         operator = val;
         toggle_btn(val);
         inputString = '';
-        console.log(num1, operator);
         return
     }
     else if (val == '=') {
-        num2 = parseFloat(inputString);
-        console.log(num2, val);
-        let final_result = calculator(num1, num2, operator);
-        console.log(final_result);
-        document.querySelector('#display').value = final_result;
-        inputString = final_result.toString();
-        toggle_btn(operator);
-        return
+        const result = eval(equation);
+        document.querySelector('#display').value = result;
+        equation = result;
+        flag = 1;
+        return;
     }
     else if (val == 'AC') {
-        console.log("reset")
+        console.log("Reset")
         document.getElementById('display').value = '0';
         // toggle_btn(operator);
         inputString = '';
         num1 = 0;
         num2 = 0;
         operator = '';
+        equation = '';
+        flag = 1;
         return;
     }
     else if (val == 'C') {
         console.log("Backspace")
         inputString = inputString.slice(0, -1);
+        equation = equation.slice(0, -1);
     }
     else if (val == '.' && inputString.includes('.')) { return }
     else if (val == '0' && len == 1 && inputString[0] == '0') { return }
     else inputString += val;
+    equation += val;
+    operator = "";
     document.querySelector('#display').value = inputString;
 }
 
